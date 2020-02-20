2e1e21329b8e77143e48f60ecadc3c4a47fdee8d
SahilPrajapati <sahilprajapati286@gmail.com>
2020-02-20 23:42:50 +0530

    interview preparation

---
+++ Trees/589. N-ary Tree Preorder Traversal.cpp
@@ -0,0 +1,61 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+/*
+class Solution {
+public:
+    void traversal(Node* root, vector<int>& result)
+    {
+        if(!root)
+            return;
+        
+        result.push_back(root->val);
+        
+        for(int i = 0; i < root->children.size(); i++)
+            traversal(root->children[i], result);
+    }
+    
+    vector<int> preorder(Node* root) {
+        
+        vector<int> result;
+        
+        traversal(root, result);
+        return result;
+    }
+};*/
+
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        stack<Node*> nodes;
+        vector<int> traversal;
+        if (root != nullptr) {
+            nodes.push(root);
+        }
+        while (!nodes.empty()) {
+            auto cur = nodes.top();
+            nodes.pop();
+            traversal.push_back(cur->val);
+            for (auto iter = cur->children.rbegin(); iter != cur->children.rend(); iter++) {
+                nodes.push(*iter);
+            }
+        }
+        return traversal;
+    }
+};
